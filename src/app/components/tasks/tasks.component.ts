a2e4958f7efd14357ec217e711f56bf1a6fe195a
Carla <carla.v.gonzalez@gmail.com>
2022-02-15 14:57:03 -0300

    CHANGELOG: [creates db.json installs json server creates add-task component creates task-item component creates task service creates ui service]

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { TaskService } from 'src/app/services/task.service';
 import { Task } from 'src/app/Task';
 
-
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
@@ -11,9 +10,26 @@ import { Task } from 'src/app/Task';
 export class TasksComponent implements OnInit {
   tasks: Task[] = [];
 
-  constructor(private taskService: TaskService) {}
+  constructor(private taskService: TaskService) {} //Para usar un servicio debemos agregarlo como provider en el constructor
 
   ngOnInit(): void {
-    this.taskService.getTasks().subscribe((tasks)=> this.tasks = tasks);
+    this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks)); //El getTastks ahora es un observable, por lo cual debemos suscribirnos a el
+  }
+
+  deleteTask(task: Task) {
+    this.taskService
+      .deleteTask(task)
+      .subscribe(
+        () => (this.tasks = this.tasks.filter((t) => t.id !== task.id))
+      );
+  }
+
+  toggleReminder(task: Task) {
+    task.reminder = !task.reminder;
+    this.taskService.updateTaskReminder(task).subscribe();
+  }
+
+  addTask(task: Task) {
+    this.taskService.addTask(task).subscribe((task) => this.tasks.push(task));
   }
 }
